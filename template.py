d44cb30af064088f4d1daab63d78ae818446ac03
unknown <OjasKelkar>
2024-05-30 16:43:05 +0530

    folder structure updated

--- template.py
+++ template.py
@@ -14,10 +14,10 @@ list_of_files=[
     "src/pipeline/__init__.py",
     "src/pipeline/training_pipeline.py",
     "src/pipeline/prediction_pipeline.py",
-    "src/utils/__init__.py"
-    "src/utils/utils.py"
-    "src/logger/logging.py"
-    "src/exception/exception"
+    "src/utils/__init__.py",
+    "src/utils/utils.py",
+    "src/logger/logging.py",
+    "src/exception/exception.py",
     "tests/unit/__init__.py",
     "tests/iniegration/__init__.py",
     "init_setup.sh",
@@ -27,7 +27,7 @@ list_of_files=[
     "setup.cfg",
     "pyproject.toml",
     "tox.ini",
-    "experiment/experiments.ipynb"
+    "experiment/experiments.ipynb",
 ]
 
 
